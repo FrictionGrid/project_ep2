d89ed8b8b6b787dd2e317051f13b8a0a27f1b039
Frictiongrid <phumiphat.s2310@gmail.com>
2026-09-20 14:50:53 +0700

    level3:layout

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import expressLayouts from "express-ejs-layouts";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(process.cwd(), 'view'));
   app.useStaticAssets(join(process.cwd(), 'public'));
   app.setViewEngine('ejs');
+  app.use(expressLayouts);
+  app.set('layout', 'layouts/layout_ep1');
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
